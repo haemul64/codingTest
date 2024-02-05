7f8a447a2cc979efd3803897c8c1d442d90783dc
haemul64 <haemul64@gmail.com>
2024-02-05 16:00:58 +0900

    daily coding test study

---
+++ baekjoon/src/silver/단어_정렬_1181.java
@@ -0,0 +1,31 @@
+package silver;
+
+import java.io.IOException;
+
+public class 단어_정렬_1181 {
+	public static void main(String[] args) throws IOException {
+		boolean[] numbers = new boolean[10001]; // false면 셀프넘버
+
+		for (int i = 1; i < 10001; i++) {
+			int number = makeNumber(i);
+			while (number < 10001 && !numbers[number]) {
+				numbers[number] = true;
+				number = makeNumber(number);
+			}
+		}
+
+		for (int i = 1; i < 10001; i++) {
+			if (!numbers[i])
+				System.out.println(i);
+		}
+	} // main
+
+	public static int makeNumber(int n) {
+		int ans = n;
+		while (n > 0) {
+			ans += n % 10;
+			n /= 10;
+		}
+		return ans;
+	}
+}
